4ed51c210dc0a5915099aa20795539a9bcbb0bf9
Mark Schumaker <m.schumaker235@gmail.com>
2024-02-22 15:08:40 -0500

    ugh

--- src/components/dash/task-edit-form.js
+++ src/components/dash/task-edit-form.js
@@ -54,14 +54,12 @@ export function TaskEditForm(task, showModal) {
                     textContent: 'Due Date'
                 }),
                 // DueDateInpout(task),
-                element('div', {className: 'date-temp-wrap'},
-                    element('input', {
-                        className: 'date-temp',
-                        type: 'date', 
-                        name: 'dueDate',
-                        value: isoToInput(task.dueDate)
-                    })
-                ),
+                element('input', {
+                    className: 'date-temp',
+                    type: 'date', 
+                    name: 'dueDate',
+                    value: isoToInput(task.dueDate)
+                }),
                 element('label', {
                     className: 'label',
                     textContent: 'Tag'
